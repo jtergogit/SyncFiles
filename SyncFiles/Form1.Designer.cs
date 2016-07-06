a4f70414d2ca031933ff18e726d80017f1569443
wjm <wjm@git>
2016-07-06 14:10:06 +0800

    吖吖

--- SyncFiles/Form1.Designer.cs
+++ SyncFiles/Form1.Designer.cs
@@ -40,6 +40,13 @@
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.projectPaths = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.exportText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.exportBtn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ignoreText = new System.Windows.Forms.TextBox();
+            this.ignoreBtn = new System.Windows.Forms.Button();
+            this.exportSelect = new System.Windows.Forms.Button();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -56,7 +63,7 @@
             this.SelectFolder.Name = "SelectFolder";
             this.SelectFolder.Size = new System.Drawing.Size(75, 23);
             this.SelectFolder.TabIndex = 1;
-            this.SelectFolder.Text = "选择文件";
+            this.SelectFolder.Text = "选择文件夹";
             this.SelectFolder.UseVisualStyleBackColor = true;
             this.SelectFolder.Click += new System.EventHandler(this.SelectFolder_Click);
             // 
@@ -72,7 +79,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(20, 53);
+            this.label2.Location = new System.Drawing.Point(32, 90);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(65, 12);
             this.label2.TabIndex = 3;
@@ -80,7 +87,7 @@
             // 
             // MoveFile
             // 
-            this.MoveFile.Location = new System.Drawing.Point(393, 85);
+            this.MoveFile.Location = new System.Drawing.Point(393, 178);
             this.MoveFile.Name = "MoveFile";
             this.MoveFile.Size = new System.Drawing.Size(75, 23);
             this.MoveFile.TabIndex = 6;
@@ -92,7 +99,7 @@
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 121);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 215);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Size = new System.Drawing.Size(501, 22);
             this.statusStrip1.TabIndex = 7;
@@ -106,14 +113,14 @@
             // 
             // projectPaths
             // 
-            this.projectPaths.Location = new System.Drawing.Point(132, 44);
+            this.projectPaths.Location = new System.Drawing.Point(132, 90);
             this.projectPaths.Name = "projectPaths";
             this.projectPaths.Size = new System.Drawing.Size(261, 21);
             this.projectPaths.TabIndex = 8;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(393, 42);
+            this.button1.Location = new System.Drawing.Point(393, 88);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 9;
@@ -121,11 +128,80 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.SelectFile_Click);
             // 
+            // exportText
+            // 
+            this.exportText.Location = new System.Drawing.Point(132, 51);
+            this.exportText.Name = "exportText";
+            this.exportText.Size = new System.Drawing.Size(195, 21);
+            this.exportText.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "导出站点：";
+            // 
+            // exportBtn
+            // 
+            this.exportBtn.Location = new System.Drawing.Point(414, 51);
+            this.exportBtn.Name = "exportBtn";
+            this.exportBtn.Size = new System.Drawing.Size(75, 23);
+            this.exportBtn.TabIndex = 12;
+            this.exportBtn.Text = "导出";
+            this.exportBtn.UseVisualStyleBackColor = true;
+            this.exportBtn.Click += new System.EventHandler(this.exportBtn_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "忽略文件：";
+            // 
+            // ignoreText
+            // 
+            this.ignoreText.Location = new System.Drawing.Point(132, 128);
+            this.ignoreText.Name = "ignoreText";
+            this.ignoreText.Size = new System.Drawing.Size(261, 21);
+            this.ignoreText.TabIndex = 14;
+            // 
+            // ignoreBtn
+            // 
+            this.ignoreBtn.Location = new System.Drawing.Point(393, 126);
+            this.ignoreBtn.Name = "ignoreBtn";
+            this.ignoreBtn.Size = new System.Drawing.Size(75, 23);
+            this.ignoreBtn.TabIndex = 15;
+            this.ignoreBtn.Text = "选择文件";
+            this.ignoreBtn.UseVisualStyleBackColor = true;
+            this.ignoreBtn.Click += new System.EventHandler(this.ignoreBtn_Click);
+            // 
+            // exportSelect
+            // 
+            this.exportSelect.Location = new System.Drawing.Point(333, 51);
+            this.exportSelect.Name = "exportSelect";
+            this.exportSelect.Size = new System.Drawing.Size(75, 23);
+            this.exportSelect.TabIndex = 16;
+            this.exportSelect.Text = "选择目录";
+            this.exportSelect.UseVisualStyleBackColor = true;
+            this.exportSelect.Click += new System.EventHandler(this.exportSelect_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(501, 143);
+            this.ClientSize = new System.Drawing.Size(501, 237);
+            this.Controls.Add(this.exportSelect);
+            this.Controls.Add(this.ignoreBtn);
+            this.Controls.Add(this.ignoreText);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.exportBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.exportText);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.projectPaths);
             this.Controls.Add(this.statusStrip1);
@@ -157,6 +233,13 @@
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.TextBox projectPaths;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox exportText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button exportBtn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ignoreText;
+        private System.Windows.Forms.Button ignoreBtn;
+        private System.Windows.Forms.Button exportSelect;
     }
 }
 
--- SyncFiles/Form1.cs
+++ SyncFiles/Form1.cs
@@ -8,16 +8,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace SyncFiles
 {
     public partial class Form1 : Form
     {
+        protected string settingPath = "Setting.config";
         public Form1()
         {
             InitializeComponent();
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(settingPath);
+            ProjectPath.Text = xmlDoc.SelectSingleNode("configuration/ProjectPath").InnerText;
+            exportText.Text = xmlDoc.SelectSingleNode("configuration/exportText").InnerText;
+            projectPaths.Text = xmlDoc.SelectSingleNode("configuration/projectPaths").InnerText;
+            ignoreText.Text = xmlDoc.SelectSingleNode("configuration/ignoreText").InnerText;
         }
 
+        /// <summary>
+        /// 选择同步项目
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void SelectFolder_Click(object sender, EventArgs e)
         {
             if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
@@ -25,18 +39,98 @@ namespace SyncFiles
                 if (!string.IsNullOrWhiteSpace(folderBrowserDialog1.SelectedPath))
                 {
                     ProjectPath.Text = folderBrowserDialog1.SelectedPath;
+                    SaveSetting("ProjectPath", ProjectPath.Text);
                 }
             }
         }
 
+        /// <summary>
+        /// 选择导出站点目录
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exportSelect_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (!string.IsNullOrWhiteSpace(folderBrowserDialog1.SelectedPath))
+                {
+                    exportText.Text = folderBrowserDialog1.SelectedPath;
+                    SaveSetting("exportText", exportText.Text);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 导出站点
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exportBtn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(exportText.Text.Trim()))
+            {
+                MessageBox.Show("请选择导出目录");
+                return;
+            }
+            StreamWriter sw = new StreamWriter("WebSites.txt");
+            string[] directionName = Directory.GetDirectories(exportText.Text);
+            foreach (string directionPath in directionName)
+            {
+                if (ProjectPath.Text != directionPath)
+                sw.WriteLine(directionPath);
+            }
+            sw.Close();
+            projectPaths.Text = Directory.GetCurrentDirectory() + "\\WebSites.txt";
+            SaveSetting("projectPaths", projectPaths.Text);
+        }   
+
+        /// <summary>
+        /// 选择同步站点文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void SelectFile_Click(object sender, EventArgs e)
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 projectPaths.Text = openFileDialog1.FileName;
+                SaveSetting("projectPaths", projectPaths.Text);
             }
         }
 
+        /// <summary>
+        /// 选择忽略文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ignoreBtn_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                ignoreText.Text = openFileDialog1.FileName;
+                SaveSetting("ignoreText", ignoreText.Text);
+            }
+        }
+
+        /// <summary>
+        /// 保存选择的路径信息
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
+        private void SaveSetting(string name, string value)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(settingPath);
+            xmlDoc.SelectSingleNode("configuration/" + name).InnerText = value;
+            xmlDoc.Save(settingPath);
+        }
+
+        /// <summary>
+        /// 开始同步文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void MoveFile_Click(object sender, EventArgs e)
         {
             if (string.IsNullOrEmpty(ProjectPath.Text))
@@ -68,57 +162,98 @@ namespace SyncFiles
                    catch(Exception ex)
                    {
                        toolStripStatusLabel1.Text = ex.Message;
+                       MessageBox.Show(ex.Message);
                    }
                });
         }
 
-         public static void copyDirectory(string sourceDirectory, string destDirectory)
+        /// <summary>
+        /// 检查忽略文件
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="ignoreFile"></param>
+        /// <returns></returns>
+        private bool IgnoreFiles(string fileName)
         {
-            //判断源目录和目标目录是否存在，如果不存在，则创建一个目录
-            if (!Directory.Exists(sourceDirectory))
+            using (StreamReader sr = new StreamReader(ignoreText.Text))
             {
-                Directory.CreateDirectory(sourceDirectory);
+                while (!sr.EndOfStream)
+                {
+                    if (Path.Equals(fileName, sr.ReadLine()))
+                    {
+                        return false;
+                    }
+                }
+                sr.Close();
             }
-            if (!Directory.Exists(destDirectory))
-            {
-                Directory.CreateDirectory(destDirectory);
-            }
-            //拷贝文件
-            copyFile(sourceDirectory, destDirectory);
-           
-            //拷贝子目录       
-            //获取所有子目录名称
-            string[] directionName = Directory.GetDirectories(sourceDirectory);
-           
-            foreach (string directionPath in directionName)
-            {
-                //根据每个子目录名称生成对应的目标子目录名称
-                string directionPathTemp = destDirectory + "\\" + directionPath.Substring(sourceDirectory.Length + 1);
-               
-                //递归下去
-                copyDirectory(directionPath, directionPathTemp);
-            }                     
+            return true;
         }
-        public static void copyFile(string sourceDirectory, string destDirectory)
+
+        /// <summary>
+        /// 遍历递归文件
+        /// </summary>
+        /// <param name="sourceDirectory"></param>
+        /// <param name="destDirectory"></param>
+         public void copyDirectory(string sourceDirectory, string destDirectory)
+        {
+            if (IgnoreFiles(sourceDirectory) && sourceDirectory != destDirectory)
+            {
+                //判断源目录和目标目录是否存在，如果不存在，则创建一个目录
+                if (!Directory.Exists(sourceDirectory))
+                {
+                    Directory.CreateDirectory(sourceDirectory);
+                }
+                if (!Directory.Exists(destDirectory))
+                {
+                    Directory.CreateDirectory(destDirectory);
+                }
+                //拷贝文件
+                copyFile(sourceDirectory, destDirectory);
+
+                //拷贝子目录       
+                //获取所有子目录名称
+                string[] directionName = Directory.GetDirectories(sourceDirectory);
+
+                foreach (string directionPath in directionName)
+                {
+                    //根据每个子目录名称生成对应的目标子目录名称
+                    string directionPathTemp = destDirectory + "\\" + directionPath.Substring(sourceDirectory.Length + 1);
+
+                    //递归下去
+                    copyDirectory(directionPath, directionPathTemp);
+                }
+            } 
+        }
+
+        /// <summary>
+        /// 复制文件
+        /// </summary>
+        /// <param name="sourceDirectory"></param>
+        /// <param name="destDirectory"></param>
+        public void copyFile(string sourceDirectory, string destDirectory)
         {
             //获取所有文件名称
             string[] fileName = Directory.GetFiles(sourceDirectory);
            
             foreach (string filePath in fileName)
             {
-                //根据每个文件名称生成对应的目标文件名称
-                string filePathTemp = destDirectory + "\\" + filePath.Substring(sourceDirectory.Length + 1);
-               
-                //若不存在，直接复制文件；若存在，覆盖复制
-                if (File.Exists(filePathTemp))
+                if (IgnoreFiles(filePath))
                 {
-                    File.Copy(filePath, filePathTemp, true);
-                }
-                else
-                {
-                    File.Copy(filePath, filePathTemp);
+                    //根据每个文件名称生成对应的目标文件名称
+                    string filePathTemp = destDirectory + "\\" +  filePath.Substring(sourceDirectory.Length + 1);;
+
+                    //若不存在，直接复制文件；若存在，覆盖复制
+                    if (File.Exists(filePathTemp))
+                    {
+                        File.Copy(filePath, filePathTemp, true);
+                    }
+                    else
+                    {
+                        File.Copy(filePath, filePathTemp);
+                    }
                 }
             }
-        }    
+        }
+ 
     }
 }
