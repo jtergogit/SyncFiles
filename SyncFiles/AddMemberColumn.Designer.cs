7b7c6ad7d2040f73c352f68f43aa12fe3acfa817
wjm <wjm@git>
2016-09-14 14:47:17 +0800

    init membercolumn

---
+++ SyncFiles/AddMemberColumn.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace SyncFiles
+{
+    partial class AddMemberColumn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddMemberColumn));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.buttonLacaConfig = new System.Windows.Forms.Button();
+            this.textLacaConfig = new System.Windows.Forms.TextBox();
+            this.labelLacaConfig = new System.Windows.Forms.Label();
+            this.labelWebConfig = new System.Windows.Forms.Label();
+            this.textWebConfig = new System.Windows.Forms.TextBox();
+            this.buttonWebConfig = new System.Windows.Forms.Button();
+            this.buttonContras = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonLacaConfig
+            // 
+            this.buttonLacaConfig.Location = new System.Drawing.Point(359, 30);
+            this.buttonLacaConfig.Name = "buttonLacaConfig";
+            this.buttonLacaConfig.Size = new System.Drawing.Size(75, 23);
+            this.buttonLacaConfig.TabIndex = 12;
+            this.buttonLacaConfig.Text = "选择文件";
+            this.buttonLacaConfig.UseVisualStyleBackColor = true;
+            this.buttonLacaConfig.Click += new System.EventHandler(this.buttonLacaConfig_Click);
+            // 
+            // textLacaConfig
+            // 
+            this.textLacaConfig.Location = new System.Drawing.Point(83, 32);
+            this.textLacaConfig.Name = "textLacaConfig";
+            this.textLacaConfig.Size = new System.Drawing.Size(270, 21);
+            this.textLacaConfig.TabIndex = 11;
+            // 
+            // labelLacaConfig
+            // 
+            this.labelLacaConfig.AutoSize = true;
+            this.labelLacaConfig.Location = new System.Drawing.Point(12, 41);
+            this.labelLacaConfig.Name = "labelLacaConfig";
+            this.labelLacaConfig.Size = new System.Drawing.Size(65, 12);
+            this.labelLacaConfig.TabIndex = 14;
+            this.labelLacaConfig.Text = "本地配置：";
+            // 
+            // labelWebConfig
+            // 
+            this.labelWebConfig.AutoSize = true;
+            this.labelWebConfig.Location = new System.Drawing.Point(12, 104);
+            this.labelWebConfig.Name = "labelWebConfig";
+            this.labelWebConfig.Size = new System.Drawing.Size(65, 12);
+            this.labelWebConfig.TabIndex = 15;
+            this.labelWebConfig.Text = "站点配置：";
+            // 
+            // textWebConfig
+            // 
+            this.textWebConfig.Location = new System.Drawing.Point(83, 95);
+            this.textWebConfig.Name = "textWebConfig";
+            this.textWebConfig.Size = new System.Drawing.Size(270, 21);
+            this.textWebConfig.TabIndex = 16;
+            // 
+            // buttonWebConfig
+            // 
+            this.buttonWebConfig.Location = new System.Drawing.Point(359, 93);
+            this.buttonWebConfig.Name = "buttonWebConfig";
+            this.buttonWebConfig.Size = new System.Drawing.Size(75, 23);
+            this.buttonWebConfig.TabIndex = 17;
+            this.buttonWebConfig.Text = "选择文件";
+            this.buttonWebConfig.UseVisualStyleBackColor = true;
+            this.buttonWebConfig.Click += new System.EventHandler(this.buttonWebConfig_Click);
+            // 
+            // buttonContras
+            // 
+            this.buttonContras.Location = new System.Drawing.Point(359, 144);
+            this.buttonContras.Name = "buttonContras";
+            this.buttonContras.Size = new System.Drawing.Size(75, 23);
+            this.buttonContras.TabIndex = 18;
+            this.buttonContras.Text = "对比";
+            this.buttonContras.UseVisualStyleBackColor = true;
+            this.buttonContras.Click += new System.EventHandler(this.buttonContras_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 170);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(444, 22);
+            this.statusStrip1.TabIndex = 19;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(92, 17);
+            this.toolStripStatusLabel1.Text = "正在准备。。。";
+            // 
+            // AddMemberColumn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 192);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.buttonContras);
+            this.Controls.Add(this.buttonWebConfig);
+            this.Controls.Add(this.textWebConfig);
+            this.Controls.Add(this.labelWebConfig);
+            this.Controls.Add(this.labelLacaConfig);
+            this.Controls.Add(this.buttonLacaConfig);
+            this.Controls.Add(this.textLacaConfig);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AddMemberColumn";
+            this.Text = "添加会员栏目（爱分销出品）";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button buttonLacaConfig;
+        private System.Windows.Forms.TextBox textLacaConfig;
+        private System.Windows.Forms.Label labelLacaConfig;
+        private System.Windows.Forms.Label labelWebConfig;
+        private System.Windows.Forms.TextBox textWebConfig;
+        private System.Windows.Forms.Button buttonWebConfig;
+        private System.Windows.Forms.Button buttonContras;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
---
+++ SyncFiles/AddMemberColumn.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace SyncFiles
+{
+    public partial class AddMemberColumn : Form
+    {
+        private DateTime expire = new DateTime(2016, 12, 1);
+        public AddMemberColumn()
+        {
+            InitializeComponent();
+            textLacaConfig.Text = @"E:\abaizx\fx";
+            textWebConfig.Text = @"C:\Users\wjm\Desktop\WebSites.txt";
+
+            //textLacaConfig.Text = @"C:\Users\Administrator\Desktop\a";
+            //textWebConfig.Text = @"C:\Users\Administrator\Desktop\mc.txt";
+
+            if (expire < DateTime.Now)
+            {
+                buttonLacaConfig.Enabled = false;
+                buttonWebConfig.Enabled = false;
+                buttonContras.Enabled = false;
+                MessageBox.Show("废铁一块！");
+                toolStripStatusLabel1.Text = "此软件已报废。";
+            }
+        }
+
+        private void buttonLacaConfig_Click(object sender, EventArgs e)
+        {
+            //if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            //{
+            //    textLacaConfig.Text = openFileDialog1.FileName;
+            //}
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (!string.IsNullOrWhiteSpace(folderBrowserDialog1.SelectedPath))
+                {
+                    textLacaConfig.Text = folderBrowserDialog1.SelectedPath;
+                }
+            }
+        }
+
+        private void buttonWebConfig_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textWebConfig.Text = openFileDialog1.FileName;
+            }
+            //if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            //{
+            //    if (!string.IsNullOrWhiteSpace(folderBrowserDialog1.SelectedPath))
+            //    {
+            //        textWebConfig.Text = folderBrowserDialog1.SelectedPath;
+            //    }
+            //}
+        }
+
+        private void buttonContras_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(textLacaConfig.Text))
+            {
+                MessageBox.Show("请选择本地配置");
+                return;
+            }
+            if (string.IsNullOrEmpty(textWebConfig.Text))
+            {
+                MessageBox.Show("请选择站点配置");
+                return;
+            }
+            string locaPath = textLacaConfig.Text + "/config/MemberMenu.xml";
+
+            XmlDocument xmlLoca = new XmlDocument();
+            xmlLoca.Load(locaPath);
+            Task.Run(() =>
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(textWebConfig.Text))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string dirName = sr.ReadLine();
+                            toolStripStatusLabel1.Text = "正在对比：" + dirName.Substring(dirName.LastIndexOf("\\") + 1);
+                            string webPath = dirName + "/config/MemberMenu.xml";
+                            if (!File.Exists(webPath))
+                            {
+                                File.Copy(locaPath, webPath);
+                                continue;
+                            }
+                            XmlDocument xmlWeb = new XmlDocument();
+                            xmlWeb.Load(webPath);
+
+                            XmlNodeList listLoca = xmlLoca.SelectSingleNode("List").ChildNodes;
+
+                            XmlNodeList listWeb = xmlWeb.SelectSingleNode("List").ChildNodes;
+
+
+                            List<string> result = new List<string>();
+                            
+                            for (int i = 0; i < listLoca.Count; i++)
+                            {
+                                XmlNodeList menus = listLoca[i].ChildNodes;
+                                foreach (XmlNode menu in menus)
+                                {
+                                    bool compare = true;
+                                    for (int y = 0; y < listWeb.Count; y++)
+                                    {
+                                        XmlNodeList webxml = listWeb[y].ChildNodes;
+                                        foreach (XmlNode nodeWeb in webxml)
+                                        {
+                                            if (menu.Attributes["name"].Value == nodeWeb.Attributes["name"].Value)
+                                            {
+                                                compare = false;
+                                                break;
+                                            }
+                                        }
+                                    }
+                                    if (compare)
+                                    {
+                                        if (menu.Attributes["name"].Value != "#comment")
+                                        {
+                                            result.Add(i + menu.Attributes["name"].Value);
+                                        }
+                                    }
+                                }
+                            }
+
+                            //WriteFile(result);
+                            if (result.Count == 0)
+                            {
+                                //MessageBox.Show("完全匹配！");
+                            }
+                            else
+                            {
+                                for (int s = 0; s < result.Count; s++)
+                                {
+                                    string ix = result[s].Substring(0, 1);
+                                    string addname = result[s].Substring(1);
+                                    XmlNode LocaRoot = xmlLoca.SelectSingleNode("List/Item[" + ix + "]/Menu[@name='" + addname + "']");
+                                    XmlElement root = xmlWeb.CreateElement(LocaRoot.Name);
+                                    root.SetAttribute("name", LocaRoot.Attributes["name"].Value);
+                                    root.SetAttribute("display", "False");
+                                    root.SetAttribute("href", LocaRoot.Attributes["href"].Value);
+                                    root.SetAttribute("img", LocaRoot.Attributes["img"].Value);
+                                    root.InnerText = LocaRoot.InnerText;
+                                    xmlWeb.SelectSingleNode("List/Item[" + ix + "]").AppendChild(root);
+                                }
+                                xmlWeb.Save(webPath);
+                            }
+                        }
+                    }
+                    toolStripStatusLabel1.Text = "栏目更新成功";
+                    MessageBox.Show("栏目更新成功！");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            });
+
+        }
+
+        private void WriteFile(string message)
+        {
+            StreamWriter sw = new StreamWriter(@"C:\Users\Administrator\Desktop\ColummnCompareResult.txt", false);
+            foreach (string item in message.Split(','))
+            {
+                if (!string.IsNullOrEmpty(item))
+                    sw.WriteLine(item);
+            }
+            sw.Close();
+        }
+    }
+}
